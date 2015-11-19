a73b9070b1f635f95c236e69c9768524c66ce850
Christopher Wright <cwright814@gmail.com>
2015-11-19 18:01:56 -0500

    UI-Router + cleanup & prep

--- portfolio/assets/angular/app.js
+++ portfolio/assets/angular/app.js
@@ -2,24 +2,63 @@
 (function() {
     'use strict';
 
-    angular
+    var app = angular
         .module('portfolio', [
-            'ui.router','ui.bootstrap', 'angular-confirm'
+            'ui.router'
         ]);
 
-    angular
-        .module('portfolio')
-        .controller('portfolioController', portfolioController);
+    app.config(cfgPortfolio);
+    app.controller('ctrlHeader', ctrlHeader);
+    app.controller('ctrlHome', ctrlHome);
+    app.controller('ctrlFooter', ctrlFooter);
 
-    portfolioController.$inject = [
+    cfgPortfolio.$inject = [
+        '$stateProvider', '$urlRouterProvider'
+    ];
+
+    ctrlHome.$inject = [
         '$scope', '$http', '$filter', '$timeout'
     ];
 
-    function portfolioController($scope, $http, $filter, $timeout) {
+    function cfgPortfolio($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl: 'templates/header.html',
+                        controller: 'ctrlHeader',
+                        controllerAs: 'header'
+                    },
+                    'content': {
+                        templateUrl: 'templates/home.html',
+                        controller: 'ctrlHome',
+                        controllerAs: 'home'
+                    },
+                    'footer': {
+                        templateUrl: 'templates/footer.html',
+                        controller: 'ctrlFooter',
+                        controllerAs: 'footer'
+                    }
+                }
+            });
+    }
+
+    function ctrlHeader() {
+        var ctrl = this;
+    }
+
+    function ctrlHome($scope, $http, $filter, $timeout) {
         var scope = $scope;
         var http = $http;
         var filter = $filter;
         var timeout = $timeout;
+        var ctrl = this;
+    }
+
+    function ctrlFooter() {
         var ctrl = this;
     }
 })();
