bc3cf91cd6641c826e28f51fe7312e4d416b1ec9
Christopher Wright <cwright814@gmail.com>
2015-11-20 23:13:40 -0500

    Hardware acceleration + pseudo 3D effect + text shadow

--- portfolio/assets/angular/app.js
+++ portfolio/assets/angular/app.js
@@ -60,27 +60,38 @@
         var particleVars = [];
 
         // Creating particles
-        for (var i = 0; i < 40; i++) {
+        for (var i = 0; i < 41; i++) {
             var temp = domParticle.clone();
-            var x = -5 + Math.random()*105;
-            var y = -5 + Math.random()*105;
-            var z, r;
+            var x = Math.random();
+            var y = Math.random();
+            var z, r, o;
 
             if (i < 25) {
                 z = 1;
                 r = 8;
+                o = 0.2;
             }
             else if (i >= 25 && i < 35) {
                 z = 2;
                 r = 24;
+                o = 0.4;
+            }
+            else if (i >= 35 && i < 40) {
+                z = 3;
+                r = 64;
+                o = 0.65;
             }
             else {
-                z = 3;
-                r = 48;
+                z = 5;
+                r = 160;
+                o = 1;
             }
 
-            temp.css('left', x + '%');
-            temp.css('top', y + '%');
+            TweenMax.to(temp, 0, {
+                x: x * domParticles.width(),
+                y: y * (300 - z*30),
+                rotation: 0.0003
+            });
             temp.css('z-index', z);
             temp.css('width', r*2);
             temp.css('height', r*2);
@@ -90,6 +101,7 @@
                     'rgba(56, 76, 102, 0) 100%' +
                 ')'
             );
+            temp.css('opacity', o);
 
             domParticles.append(temp);
             particles.push(temp);
@@ -101,21 +113,23 @@
         domParticle.remove();
 
         // Begin particle animation
-        $.each(particles, function(i, particle) {
+        // $.each(particles, function(i, particle) {
+        for (var i = 0; i < particles.length; i++) {
+            var particle = particles[i];
             var x = particleVars[i][0];
             var y = particleVars[i][1];
             var z = particleVars[i][2];
             particle.css('display', 'inline');
             updateParticle(particle, x, y, z, Math.random()*2.5);
-        });
+        }
 
         // Update particle animation
         function updateParticle(particle, x, y, z, delay) {
             TweenMax.to(particle, 2.5, {
-                left: x + z * (-1.5 + Math.random()*3) + '%',
-                top: y + z * (-1.5 + Math.random()*3) + '%',
+                x: (x + z * (-0.015 + Math.random()*0.03)) * domParticles.width(),
+                y: (y + z * (-0.015 + Math.random()*0.03)) * (300 - z*30),
                 delay: delay,
-                ease: Sine.easeInOut,
+                ease: Power1.easeInOut,
                 onComplete: updateParticle,
                 onCompleteParams: [particle, x, y, z, 0]
             });
