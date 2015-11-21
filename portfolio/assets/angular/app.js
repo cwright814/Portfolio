deaca1a5be9d1ef84d2a20616c918d29d2624b3e
Christopher Wright <cwright814@gmail.com>
2015-11-20 21:29:04 -0500

    Working particles using left/top

--- portfolio/assets/angular/app.js
+++ portfolio/assets/angular/app.js
@@ -54,18 +54,72 @@
         var timeout = $timeout;
         var ctrl = this;
 
-        var particles = $('#particles');
-        var particle = $('#particle');
+        var domParticles = $('#particles');
+        var domParticle = $('#particle');
+        var particles = [];
+        var particleVars = [];
 
-        for (var i = 0; i < 20; i++)
-        {
-            var temp = particle.clone();
-            temp.css('left', -5 + Math.random()*105 + '%');
-            temp.css('top', -5 + Math.random()*105 + '%');
-            particles.append(temp);
+        // Creating particles
+        for (var i = 0; i < 40; i++) {
+            var temp = domParticle.clone();
+            var x = -5 + Math.random()*105;
+            var y = -5 + Math.random()*105;
+            var z, r;
+
+            if (i < 25) {
+                z = 1;
+                r = 8;
+            }
+            else if (i >= 25 && i < 35) {
+                z = 2;
+                r = 24;
+            }
+            else {
+                z = 3;
+                r = 48;
+            }
+
+            temp.css('left', x + '%');
+            temp.css('top', y + '%');
+            temp.css('z-index', z);
+            temp.css('width', r*2);
+            temp.css('height', r*2);
+            temp.css('background', 'radial-gradient(' +
+                    'circle ' + r + 'px,' +
+                    'rgba(56, 76, 102, 0.75) 0%,' +
+                    'rgba(56, 76, 102, 0) 100%' +
+                ')'
+            );
+
+            domParticles.append(temp);
+            particles.push(temp);
+            particleVars.push([]);
+            particleVars[i].push(x);
+            particleVars[i].push(y);
+            particleVars[i].push(z);
         }
-        particle.remove();
-        particles.children().css('display', 'inline');
+        domParticle.remove();
+
+        // Begin particle animation
+        $.each(particles, function(i, particle) {
+            var x = particleVars[i][0];
+            var y = particleVars[i][1];
+            var z = particleVars[i][2];
+            particle.css('display', 'inline');
+            updateParticle(particle, x, y, z, Math.random()*2.5);
+        });
+
+        // Update particle animation
+        function updateParticle(particle, x, y, z, delay) {
+            TweenMax.to(particle, 2.5, {
+                left: x + z * (-1.5 + Math.random()*3) + '%',
+                top: y + z * (-1.5 + Math.random()*3) + '%',
+                delay: delay,
+                ease: Sine.easeInOut,
+                onComplete: updateParticle,
+                onCompleteParams: [particle, x, y, z, 0]
+            });
+        }
     }
 
     function ctrlHome($scope, $http, $filter, $timeout) {
